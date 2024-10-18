34b29989fb6dea7eb585a84d3078b773bc8b8781
Tanaya Butala <141510786+Tanxya@users.noreply.github.com>
2024-10-18 22:15:13 +0530

    Create cgpa.py

---
+++ pages/parameters/cgpa.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+def main():
+    st.title("Cumulative Grade Point Average (CGPA)")
+    st.markdown("""
+    ### CGPA (Cumulative Grade Point Average)
+
+    **CGPA** is a numerical representation (0.0 to 10.0) of your overall academic performance.
+
+    #### Importance:
+    - Reflects your academic abilities and consistency over your course of study.
+    - Often used as a key criterion for admissions to academic programs.
+    - Indicates your ability to manage coursework and maintain a high level of performance.
+
+    #### Factors Influencing CGPA:
+    - **Course Grades**: The grades you receive in individual courses.
+    - **Credit Hours**: The weight of each course based on its credit hours.
+    - **Course Difficulty**: The level of difficulty of the courses you have taken.
+
+    #### Tips:
+    - Maintain consistent performance throughout your academic career.
+    - Seek help and resources if you are struggling with certain subjects.
+    - Balance your course load to manage stress and workload effectively.
+    - Participate in study groups and seek feedback from professors to improve your understanding.
+    """)
+
+if __name__ == "__main__":
+    main()
